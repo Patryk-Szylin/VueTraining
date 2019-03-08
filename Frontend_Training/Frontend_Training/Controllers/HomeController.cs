c14a0bd2b1640be19524ac7ae17adf41aa7bbc17
Patryk Szylin <patrykszylin93@gmail.com>
2019-03-08 19:48:42 +0000

    Added images to the list of businesses listing

--- Frontend_Training/Frontend_Training/Controllers/HomeController.cs
+++ Frontend_Training/Frontend_Training/Controllers/HomeController.cs
@@ -19,7 +19,21 @@ namespace Frontend_Training.Controllers
         {
             ViewData["Message"] = "Your application description page.";
 
-            return View();
+            List<GameInstance> linksToImages = new List<GameInstance>
+            {
+                new GameInstance {link= "/images/Logos/dota-2-logo.png", companyName= "Valve", gameName= "Dota2" },
+                new GameInstance{link= "/images/Logos/League-of-legends-logo.png", companyName= "Riot Games", gameName= "League of Legends" },
+                new GameInstance{link= "/images/Logos/ClashofClans_Title.png", companyName= "Valve", gameName= "Clash of Clans" },
+                new GameInstance{link= "/images/Logos/Clash-royal-logo.png", companyName= "Valve", gameName= "Clash Royale" },
+                new GameInstance{link= "/images/Logos/Heroes_of_the_Storm_logo_2017.png", companyName= "Blizzard Entertainment", gameName= "Heroes of the Storm" },
+                new GameInstance{link= "/images/Logos/pubg_PNG33.png", companyName= "Valve", gameName= "Player Unknown's Battleground" },
+                new GameInstance{link= "/images/Logos/smite-logo-hzcom1.png", companyName= "HZCom", gameName= "Smite" },
+                new GameInstance{link= "/images/Logos/Strife_Logo.jpg", companyName= "Valve", gameName= "Strife" }
+
+            };
+
+
+            return View(linksToImages);
         }
 
         public IActionResult Contact()
---
+++ Frontend_Training/Frontend_Training/Models/GameInstance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Frontend_Training.Models
+{
+    public class GameInstance
+    {
+        public string link { get; set; }
+        public string gameName { get; set; }
+        public string companyName { get; set; }
+    }
+}
